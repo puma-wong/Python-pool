579e3fa8340297daab423e432d1d8655583c87ba
puma-wong <weibiao.h@gmail.com>
2012-04-08 15:18:14 +0800

    test python

---
+++ mysrc/method.py
@@ -0,0 +1,10 @@
+class Person:
+    def sayHi(self):
+        print 'Hello, how are you?'
+
+    def sayother(self):
+        print 'say other words~'
+        
+p = Person()
+p.sayHi()
+p.sayother()
