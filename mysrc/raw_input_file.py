e57edd2ed90052715655e2fc6228430c188b9d58
Puma Wong <weibiao.h@gmail.com>
2012-04-18 15:45:39 +0800

    for循环

---
+++ mysrc/raw_input_file.py
@@ -0,0 +1,5 @@
+a = open('a.txt','w')
+s = raw_input('enter the words:')
+print s
+a.write (s)
+a.close()
---
+++ mysrc/useing_for.py
@@ -0,0 +1,4 @@
+print 'i like to use the Internet for:'
+for item in ['e-mail','net-surfing','homework','chat']:
+    print item,
+print
